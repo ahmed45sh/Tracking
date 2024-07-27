bcf32d5b10d72581c5a1ba90bd28f32291513847
AHMED SAID HASSAN <96662587+ahmed45sh@users.noreply.github.com>
2024-07-27 13:33:03 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {
+
+      fontFamily:{
+        cairo: "Cairo",
+        montserrat: "'Montserrat', sans-serif",
+      }
+    },
+  },
+  plugins: [require('preline/plugin')],
+}
